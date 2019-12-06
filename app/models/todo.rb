4188f0b463ca9806f7a1bc6ea9edc57de34dd07e
dionomusuko <dandybird1017@gmail.com>
2019-12-07 01:29:16 +0900

    change todo.rb

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,3 @@
 class Todo < ApplicationRecord
+  validates :body, presence: true
 end
--- db/migrate/20191206162706_create_todos.rb
+++ db/migrate/20191206162706_create_todos.rb
@@ -2,7 +2,7 @@ class CreateTodos < ActiveRecord::Migration[5.2]
   def change
     create_table :todos do |t|
       t.string :body
-      t.boolean :done
+      t.boolean :done, default: false, null: false
 
       t.timestamps
     end
