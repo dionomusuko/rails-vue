bb6ce120d9c3ec0d65bb10493a3cee463673c0d9
dionomusuko <dandybird1017@gmail.com>
2019-12-07 01:48:23 +0900

    add vue change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
   namespace :api, {format: "json"} do
     namespace :v1 do
       resources :todos, :only => %i[index create update destroy]
